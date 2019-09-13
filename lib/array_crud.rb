5f18bca5b84994ca3be93423817288af0c4b7bc4
Zhari Clay <zhariclay@gmail.com>
2019-09-13 16:22:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   add_element_to_start_of_array = ["glasses", "hat", "shirt"]
-  
+  add_element_to_start_of_array.unshift
 end
 
 def remove_element_from_end_of_array(array)
