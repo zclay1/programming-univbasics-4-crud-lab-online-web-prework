12cf5efc7dcf10214d74b38028a3fd1729e033cc
Zhari Clay <zhariclay@gmail.com>
2019-09-13 18:37:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -49,6 +49,6 @@ end
 
 def update_element_from_index(array, index_number, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array[3] = "totally" => "totally"
+array[3] = "totally"
 p array
 end
