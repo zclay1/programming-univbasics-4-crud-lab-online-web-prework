badf2b31d4eb55d0056efd8b989e09802c012a34
Zhari Clay <zhariclay@gmail.com>
2019-09-13 17:02:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["wow", "I", "am", "really" "learning"]
+  add_element_to_end_of_array = ["wow", "I", "am", "really" "learning"]
   add_element_to_end_of_array << "arrays!"
   p add_element_to_end_of_array
 end
