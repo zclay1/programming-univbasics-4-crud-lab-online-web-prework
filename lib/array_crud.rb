8f5cf56783a69a8b5b44ceba8d817ddd074f0e7c
Zhari Clay <zhariclay@gmail.com>
2019-09-13 16:18:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array = ["pajamas", "makeup"]
+  add_element_to_end_of_array = ["pajamas", "makeup", "hairbrush"]
   add_element_to_end_of_array.push ("deodorant")
+  p add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(array, element)
