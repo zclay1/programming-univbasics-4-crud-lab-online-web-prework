dfa7670c2646413af82767b2534361dc0bcb34ac
Zhari Clay <zhariclay@gmail.com>
2019-09-13 17:04:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array = ["I", "am", "really", "learning"] 
+  add_element_to_start_of_array = ["I", "am", "really", "learning"]
+  
 end
 
 def remove_element_from_end_of_array(array)
