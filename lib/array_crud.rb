42538d93d04d25bdf50a93f20785e30e5e4ed422
Zhari Clay <zhariclay@gmail.com>
2019-09-13 16:33:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,9 +19,10 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element_from_end_of_array = ["arrays!", "item2"]
+  remove_element_from_end_of_array = ["item1", "arrays!"]
   item2 = remove_element_from_end_of_array.pop
-  p remove_element_from_end_of_array 
+  p remove_element_from_end_of_array
+  p 
   
 end
 
