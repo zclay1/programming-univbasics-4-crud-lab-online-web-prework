8c4ab52ca116798e3d2ca4dfa7157440733e4fbe
Zhari Clay <zhariclay@gmail.com>
2019-09-13 16:28:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,6 +19,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  remove_element_from_end_of_array = ["item1", "item2", "item3", "item4"]
+  
   
 end
 
