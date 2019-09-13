38dc84edc8f822781ebc068024aec6901ba4a0f9
Zhari Clay <zhariclay@gmail.com>
2019-09-13 16:50:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,9 @@ end
 
 def remove_element_from_end_of_array(array)
   remove_element_from_end_of_array = ["arrays!"]
-  arrays!
+  arrays! = remove_element_from_end_of_array.pop
+  p remove_element_from_end_of_array
+  p arrays!
 end
 
 def remove_element_from_start_of_array(array)
