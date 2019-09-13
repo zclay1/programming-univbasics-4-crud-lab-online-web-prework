e3812a70312fa8279eb088f5d2f0e8b9385ff2e2
Zhari Clay <zhariclay@gmail.com>
2019-09-13 16:14:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["pajamas", "toothbrush", "hairbrush", "makeup"]
+  ["pajamas", "makeup"]
   add_element_to_end_of_array << "deodorant"
 end
 
