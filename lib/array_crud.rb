c73fb9548b86456f250837e62313abd2c1e018af
Zhari Clay <zhariclay@gmail.com>
2019-09-13 18:23:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,10 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  "wow" = array.unshift
+  p array
+  p "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
